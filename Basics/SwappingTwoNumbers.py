5a7dc3c14311f779b613c2e9a033a067ae97e775
Abrar Ateeq <akabrar5@gmail.com>
2024-12-21 19:23:56 +0530

    Add files via upload

---
+++ Basics/SwappingTwoNumbers.py
@@ -0,0 +1,16 @@
+
+a = 100
+b = 20
+temp = a
+a = b
+b = temp
+print(a)
+print(b)
+
+#OR
+
+a = 100
+b = 20
+a,b=b,a
+print(a)
+print(b)
---
+++ Basics/basics.py
@@ -0,0 +1,69 @@
+#basic data types
+# a = 'abrar'
+# b = 6.5646
+# c= "jiya"
+# d = True
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+
+#id function gives you the memory address of the variable and type func gives u the type of the data type
+#we can even assign the variables like this- x,y,z=1,2,3
+#type function gives the type of the data type of the variable
+
+# a = 10
+# print(type(a))
+# b = ('gfg')
+# print(type(b))
+# c = [1,2,3,'afas']
+# print(type(c))
+# d = (10,20,30)
+# print(type(d))
+# e = {10,20,30}
+# print(type(e))
+# f = {10:'gfg', 2:'abrar'}
+# print(type(f))
+
+#List introduction
+
+# l = [10,20,30,40,50]
+# l.append(30)
+# print(l)
+# l.insert(1,15)
+# print(l)
+# print(15 in l)
+# print(l.count(30))
+# l.remove(15)
+# print(l)
+# print(l.pop())
+# print(l.pop(1))
+# del l[0:2]
+# print(l)
+# l.sort()
+# print(l)
+
+#Tuples introduction
+
+# t = (10,20,'gfg')
+# print(t)
+# print(type(t))
+# print(t[1])
+
+# t = 10,20,30,40,10
+# print(t[2])
+# print(t[-1])
+# print(t[1:3])
+# print(len(t))
+# print(t.count(10))
+# print(t.index(20))
+# print(t[::-1]) #reverse
+
+#set introduction
+# s = {10,20,30}
+# print(s)
+# s.update({40,50})
+# print(s)
+# s.clear()
+# print(s)
