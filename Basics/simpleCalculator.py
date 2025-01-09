66733fa6fd7597e7ea58964a03c5fcb82ae8f794
Abrar Ateeq <akabrar5@gmail.com>
2025-01-09 18:09:09 +0530

    Add files via upload

---
+++ Basics/simpleCalculator.py
@@ -0,0 +1,13 @@
+#simple calculator 
+a1=float(input('Enter the first number:'))
+a2=float(input('Enter the second number:'))
+
+sum = a1+ a2
+difference = a1-a2
+product = a1 *a2
+division = a1/a2
+
+print('sum: ', sum)
+print('difference: ', difference)
+print('product: ', product)
+print('division: ', division)
